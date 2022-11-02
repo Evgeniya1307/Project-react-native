ce41356d42a81f2941924f2a83118cb9352a5f5b
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-02 13:59:31 +0400

    screenOptions={{headerShown:false}}>{/*экран опций равен объекту где ус-да заголовок значения  */}

--- App.js
+++ App.js
@@ -1,35 +1,31 @@
-import { createStackNavigator } from '@react-navigation/stack';//возможность перехода между экранами, где каждый новый экран помещается поверх стека
-import {  Text, View } from 'react-native';
-import { NavigationContainer, DefaultTheme} from '@react-navigation/native';//NavigationContainer-за управление состоянием приложения и привязку навигатора верхнего уровня к среде приложения.
-import { useFonts } from 'expo-font';//шрифты
+import { createStackNavigator } from "@react-navigation/stack"; //возможность перехода между экранами, где каждый новый экран помещается поверх стека
+import { Text, View } from "react-native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native"; //NavigationContainer-за управление состоянием приложения и привязку навигатора верхнего уровня к среде приложения.
+import { useFonts } from "expo-font"; //шрифты
 
 const Stack = createStackNavigator();
 
-const theme={ //создалa тему будут распространяться все эл.темы по умолчанию
-  ...DefaultTheme
+const theme = { //создалa тему будут распространяться все эл.темы по умолчанию
+  ...DefaultTheme,
+  colors:{//доступ к цветам}
+    ...DefaultTheme.colors,
+background:"transparent"//изменяю фон
+  }
 }
 
-const App=()=> {
+
+const App = () => {
   return (
-    <NavigationContainer>
-   <StackNavigator>
-   <Stack.Screen>
-   </Stack.Screen>
-   </StackNavigator> 
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown:false}}>{/*экран опций равен объекту где ус-да заголовок значения  */}
+        <Stack.Screen />
+        <Stack.Screen />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
-
-
-
-
-
-
-
-
-
 
 // const styles = StyleSheet.create({
 //   container: {
