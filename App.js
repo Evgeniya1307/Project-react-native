06c9576b49d3da91d8cb2beaa63efd37788edba0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-07 12:37:30 +0400

    if(!loaded) return null;{ //если не загружен то null

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { createStackNavigator } from "@react-navigation/stack"; //возможн
 import { Text, View } from "react-native";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native"; //NavigationContainer-за управление состоянием приложения и привязку навигатора верхнего уровня к среде приложения.
 import { useFonts } from "expo-font"; //шрифты
+import Home from "./screens/Home";
+import Details from "./screens/Details";
 
 const Stack = createStackNavigator();
 
@@ -15,11 +17,24 @@ background:"transparent"//изменяю фон
 
 
 const App = () => {
+  const[loaded]=useFonts({//использую шрифты
+InterBold:require("./assets/Inter-Bold.ttf"),
+InterSemiBold:require("./assets/Inter-SemiBold.ttf"),
+InterMedium:require("./assets/Inter-Medium.ttf"),
+InterRegular:require("./assets/Inter-Regular.ttf"),
+InterLight:require("./assets/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null;{ //если не загружен то null
+
+  }
+
   return (
     <NavigationContainer theme={theme}>
-      <Stack.Navigator screenOptions={{headerShown:false}}>{/*экран опций равен объекту где ус-да заголовок значения  */}
-        <Stack.Screen />
-        <Stack.Screen />
+      <Stack.Navigator screenOptions={{headerShown:false}}
+      initialRouteName="Home">{/*экран опций равен объекту где ус-да заголовок значения  начальное имя маршрута  */}
+        <Stack.Screen name="Home" component={Home}/>{/*первый маршрут */}
+        <Stack.Screen name="Details" component={Details} />{/*name="Details"-детали components-подробности*/ }
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Text>Home</Text>
+  )
+}
+
+export default Home;
