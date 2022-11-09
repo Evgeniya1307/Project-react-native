e2aae6b029196a8b0723db1f252d7bbe444dcc4f
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-09 14:01:15 +0400

    Bid placed by {bid.name} {/*Ставка размещена */}

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,13 +1,43 @@
 //подробности ставки
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { EthPrice } from "./SubInfo";
+import { COLORS, SIZES, FONTS } from "../constants";
 
-import { View, Text } from 'react-native'
-import React from 'react'
-
-export const DetailsBid = () => {
+ const DetailsBid = ({ bid }) => {
+  //принимает фактическую ставку
   return (
     <View>
-      <Text>DetailsBid</Text>
-    </View>
-  )
-}
+      <Image
+        source={bid.image}
+        resizeMode="contain"
+        style={{ width: 48, height: 48 }} //фото людей делающих ставки
+      />
+      <View>
+        <Text
+          style={{
+            fontFamily: FONTS.semiBold,
+            fontSize: SIZES.small,
+            color: COLORS.primary,
+          }}
+        >
+          Bid placed by {bid.name} {/*Ставка размещена */}
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.regular,
+            fontSize: SIZES.small - 2,
+            color: COLORS.secondary,
+            marginTop: 3,
+          }}
+        >
+          {bid.date}
+        </Text>
+      </View>
 
+      <EthPrice price={bid.price} />
+    </View>
+  );
+};
+
+export default DetailsBid;
